fb38e326be6b5e99d12b19fe99fa95ae2e773021
PS1717 <38763217+Priyabrat1717@users.noreply.github.com>
2019-10-02 06:40:53 +0530

    LS-5002

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,4 +1,4 @@
 # Recovery
 TARGET_RECOVERY_DENSITY := xhdpi
-TARGET_RECOVERY_FSTAB := device/LYF/mobee01a/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := device/LYF/mirage01a/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
